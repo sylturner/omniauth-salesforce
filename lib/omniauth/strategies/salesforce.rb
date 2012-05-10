03eda95b0c000f1a356c8a1aeb2df0a86ac1e29e
Syl Turner <syl.turner@hannonhill.com>
2012-05-10 10:59:29 -0400

    Removing the signature check because it's not working for me (maybe related to REE 1.8.7?).

--- lib/omniauth/strategies/salesforce.rb
+++ lib/omniauth/strategies/salesforce.rb
@@ -31,12 +31,7 @@ module OmniAuth
         super
       end
 
-      def auth_hash
-        signed_value = access_token.params['id'] + access_token.params['issued_at']
-        raw_expected_signature = OpenSSL::HMAC.digest('sha256', options.client_secret, signed_value)
-        expected_signature = Base64.strict_encode64 raw_expected_signature
-        signature = access_token.params['signature']
-        fail! "Salesforce user id did not match signature!" unless signature == expected_signature
+      def auth_hash        
         super
       end
 
